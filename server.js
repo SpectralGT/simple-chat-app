e399dd03a49a69cca58b929ad617b53d6ee179ea
Atharv Singh <atharvsingh1396@gmail.com>
2021-12-07 16:02:45 +0530

    commit

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const serveStatic = require('serve-static');
+app.use(express.static(path.join(__dirname,"./client/build")));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+})
+
+const WebSocket = require('ws');
+
+const wss = new WebSocket.Server({port:8080});
+console.log("server started");
+wss.on('connection', (ws) => {
+  ws.on('message', (data) => {
+    wss.clients.forEach(function each(client) {
+      if (client !== ws && client.readyState === WebSocket.OPEN) {
+        client.send(JSON.stringify(JSON.parse(data)));
+        // console.log('data', JSON.parse(data));
+      }
+    });
+  });
+});
+
+app.listen(5000);
