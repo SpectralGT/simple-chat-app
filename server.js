63a754901794b3681539943cfeb9002ee9e30a1a
Atharv Singh <atharvsingh1396@gmail.com>
2021-12-07 19:44:07 +0530

    c

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const serveStatic = require('serve-static');
+const port = process.env.PORT || 5000;
 app.use(express.static(path.join(__dirname,"./client/build")));
 
 app.get('/', (req, res) => {
@@ -23,4 +23,4 @@ wss.on('connection', (ws) => {
   });
 });
 
-app.listen(5000);
+app.listen(port);
